76bc6ca73651d603e3dcfe115436801bf6de43c7
Q <andrea@pushthemonkey.com>
2013-06-28 12:17:14 +0100

    add GwenSample

---
+++ GwenSample/xcode/build/GwenSample.build/Debug/GwenSample.build/Objects-normal/i386/events.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../../Cinder-Gwen/gwen/gwen/src/events.cpp \
+  ../../../../Cinder-Gwen/gwen/gwen/include/Gwen/Events.h \
+  ../../../../Cinder-Gwen/gwen/gwen/include/Gwen/Exports.h \
+  ../../../../Cinder-Gwen/gwen/gwen/include/Gwen/Structures.h \
+  ../../../../Cinder-Gwen/gwen/gwen/include/Gwen/TextObject.h \
+  ../../../../Cinder-Gwen/gwen/gwen/include/Gwen/Utility.h \
+  ../../../../Cinder-Gwen/gwen/gwen/include/Gwen/ControlList.h
---
+++ GwenSample/xcode/build/GwenSample.build/Debug/GwenSample.build/Objects-normal/i386/vector.d
@@ -0,0 +1,12 @@
+dependencies: \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/vector.c \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/fft.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/libxtract.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_scalar.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_vector.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_delta.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_types.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_macros.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_helper.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract_macros_private.h \
+  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract_globals_private.h
