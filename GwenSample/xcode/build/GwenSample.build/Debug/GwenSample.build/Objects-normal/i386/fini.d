c7b86d9fe0965710f5a7d4d9e0b42c3fbb2ad8b0
Q <andrea@pushthemonkey.com>
2013-07-01 11:04:48 +0100

    remove duplicate sample

--- GwenSample/xcode/build/GwenSample.build/Debug/GwenSample.build/Objects-normal/i386/fini.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/fini.c \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/libxtract.h \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_scalar.h \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_vector.h \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_delta.h \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_types.h \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_macros.h \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract/xtract_helper.h
--- GwenSample/xcode/build/GwenSample.build/Debug/GwenSample.build/Objects-normal/i386/window.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/window.c \
-  /Users/Q/Code/Cinder/blocks/ciLibXtract/samples/GwenSample/xcode/../../../src/xtract_window_private.h
